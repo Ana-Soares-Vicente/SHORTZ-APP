ec6f10e8a377bd1f9dc78cc0743e63c993e81eb1
Ana-Soares-Vicente <a.vicente@catolicasc.edu.br>
2026-03-05 19:35:29 -0300

    mudanças

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bem vindo!' });
+  console.log("app inicializado e respondendo");
 });
 
+router.get('/register', function(req, res, next){
+  res.render('register', {title: 'Criar conta'});
+});
 module.exports = router;
